cdc176619674df54c16f4fff4611e911b4e12c86
zakhida1 <zakhida.nasiridinova@alatoo.edu.kg>
2025-04-09 16:45:12 +0600

    Update Task.java

--- src/Task.java
+++ src/Task.java
@@ -1,1 +1,45 @@
+public class Task {
+    private String title;
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", isCompleted=" + isCompleted +
+                '}';
+    }
+}
 
