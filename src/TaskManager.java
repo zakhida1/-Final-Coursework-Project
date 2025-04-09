b3c86b301d970c104ff1deea3ef2116a1e10a0ca
zakhida1 <zakhida.nasiridinova@alatoo.edu.kg>
2025-04-09 16:46:36 +0600

    Update TaskManager.java

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,1 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManager {
+    private List<Task> tasks = new ArrayList<>();
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void removeTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.remove(index);
+        }
+    }
+
+    public List<Task> getAllTasks() {
+        return tasks;
+    }
+
+    public void completeTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.get(index).setCompleted(true);
+        }
+    }
+
+    public void printTasks() {
+        for (Task task : tasks) {
+            System.out.println(task);
+        }
+    }
+}
 
