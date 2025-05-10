49e0aa155a12564336527f67b16e21457d6ac6e6
zakhida1 <zakhida.nasiridinova@alatoo.edu.kg>
2025-05-11 01:12:59 +0600

    Update Task.java

--- src/Task.java
+++ src/Task.java
@@ -1,45 +1,30 @@
 public class Task {
     private String title;
     private String description;
-    private boolean isCompleted;
+    private String dueDate;
+    private boolean completed;
 
-    public Task(String title, String description) {
+    public Task(String title, String description, String dueDate, boolean completed) {
         this.title = title;
         this.description = description;
-        this.isCompleted = false;
+        this.dueDate = dueDate;
+        this.completed = completed;
     }
 
-    public String getTitle() {
-        return title;
-    }
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public String getDueDate() { return dueDate; }
+    public boolean isCompleted() { return completed; }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isCompleted() {
-        return isCompleted;
-    }
-
-    public void setCompleted(boolean isCompleted) {
-        this.isCompleted = isCompleted;
-    }
+    public void setTitle(String title) { this.title = title; }
+    public void setDescription(String description) { this.description = description; }
+    public void setDueDate(String dueDate) { this.dueDate = dueDate; }
+    public void setCompleted(boolean completed) { this.completed = completed; }
 
     @Override
     public String toString() {
-        return "Task{" +
-                "title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", isCompleted=" + isCompleted +
-                '}';
+        return "[Title: " + title + " | Description: " + description + 
+               " | Due: " + dueDate + " | Done: " + (completed ? "Yes" : "No") + "]";
     }
 }
 
